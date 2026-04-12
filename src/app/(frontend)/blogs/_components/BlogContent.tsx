877b14d2595cfc6df8b06d176b8b659b16fb6d8e
adriandotdev <marcelonads@gmail.com>
2026-04-13 00:26:05 +0800

    setup tailwindcss

--- src/app/(frontend)/blogs/_components/BlogContent.tsx
+++ src/app/(frontend)/blogs/_components/BlogContent.tsx
@@ -5,7 +5,7 @@ import { RichText } from '@payloadcms/richtext-lexical/react'
 
 export const BlogContent = ({ blog }: { blog: Blog }) => {
   return (
-    <div>
+    <div className="blog-content">
       <h1>{blog.title}</h1>
       <RichText data={blog.Content} />
     </div>
